c10d7f240201b160b6399b040923f770f411b222
William Antonio <william.a.siqueira@gmail.com>
2013-12-05 19:22:45 -0200

    Adicionando JAX-RS activator e removendo declarações do web.xml

---
+++ src/main/java/org/spider/bus/util/JaxRSActivator.java
@@ -0,0 +1,8 @@
+package org.spider.bus.util;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class JaxRSActivator extends Application {
+}
